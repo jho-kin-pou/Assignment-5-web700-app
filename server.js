d4fafbdda0370a48f89d2b9319b9e97e03150606
jho-kin-pou <jeffery.hkp@gmail.com>
2024-07-24 18:31:52 -0400

    Commit 2

--- server.js
+++ server.js
@@ -70,7 +70,7 @@ app.post('/students/add', (req, res) => {
 
 app.post("/student/update", (req, res) => {
     collegeData.updateStudent(req.body)
-        .then(() => res.redirect("/students"))
+        .then(() => { res.redirect("/students") })
         .catch(err => {
             res.status(500).send("Unable to update student");
         });
